58eff2744f96eac31e42b9547f05249142d068af
Marcell Ziegler <marcell.ziegler.22@gmail.com>
2023-09-19 12:05:14 +0200

    Change Serial baudrate to 115200

--- arduino/Blink/Blink.ino
+++ arduino/Blink/Blink.ino
@@ -120,7 +120,7 @@ void setup()
     startButton.state = LOW;
 
     // Open serial communications and wait for port to open:
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial)
     {
         ; // wait for serial port to connect. Needed for native USB port only
